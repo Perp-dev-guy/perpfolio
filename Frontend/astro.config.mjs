5e317710020c68b79eee5b36687854792ba64c39
= <=>
2026-06-10 19:11:42 -0400

    modified:   Frontend/astro.config.mjs

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://perp-dev-guy.github.io/',
-  base: '/perpfolio.github.io/',
+  site: 'https://perp-dev-guy.github.io',
+  base: '/perpfolio.github.io',
   vite: {
     plugins: [tailwindcss()]
   },

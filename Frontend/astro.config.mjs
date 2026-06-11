abd25b86548e43db60188d4519eb695a8a003f79
= <=>
2026-06-11 07:35:46 -0400

    redeployment please

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://perp-dev-guy.github.io',
-  base: '/perpfolio.github.io',
+  base: 'perpfolio.github.io',
   vite: {
     plugins: [tailwindcss()]
   },

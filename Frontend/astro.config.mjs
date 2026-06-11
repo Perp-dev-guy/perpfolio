62be58e6b4ae03d73b10eac5307b83490a79517b
= <=>
2026-06-11 08:10:29 -0400

    removed prepended slash in base

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://perp-dev-guy.github.io',
-  base: '/perpfolio',
+  base: 'perpfolio',
   vite: {
     plugins: [tailwindcss()]
   },
